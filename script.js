e1949ab7c259835f0437ec739a857a8b4ffe8b30
Brent-the-carpenter <beh337@gmail.com>
2023-12-05 22:39:07 -0500

    added all buttons/ added styles to calculator

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// part one
 function addition(a,b){
     console.log (a + b) ;
 }
@@ -12,4 +14,41 @@ function multiply (a,b){
 
 function divide(a, b){
     console.log(a / b)
+}
+
+// part 2 
+
+function numberOne(x){
+
+}
+
+function operator(){
+
+}
+
+function numberTwo(y){
+
+}
+
+
+// step three
+
+function operate( operator , numberOne ,numberTwo){
+    const result = 0;
+    parseFloat(numberOne);
+    parseFloat(numberTwo);
+    switch (operator){
+        case  '+':
+            result= addition(numberOne , numberTwo);
+            break;
+        case '-':
+            result = subtraction(numberOne , numberTwo) ;
+            break;
+        case '*':
+            result = multiply(numberOne, numberTwo)  ;
+            break;
+        case '/':
+            result = divide(numberOne, numberTwo) 
+    }
+    console.log(result);
 }
